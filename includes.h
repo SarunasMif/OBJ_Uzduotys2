4c79463a11aaf4f4e5f698a0e3e90df8a3e2b0ea
SarunasMif <159712517+SarunasMif@users.noreply.github.com>
2024-04-12 16:12:47 +0300

    Add files via upload

---
+++ includes.h
@@ -0,0 +1,20 @@
+#ifndef INCLUDES_H
+#define INCLUDES_H
+
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+#include <random>
+#include <sstream>
+#include <chrono>
+#include <fstream>
+#include <list>
+#include <deque>
+#include <cstdio>
+
+using namespace std;
+using namespace std::chrono;
+
+#endif 
